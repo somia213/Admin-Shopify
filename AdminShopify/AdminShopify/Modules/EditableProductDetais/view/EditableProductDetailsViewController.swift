ac5ac60339eb0b8874a2ed7c9e0f737e9c4962cc
SomiaAmer <105312434+somia213@users.noreply.github.com>
2024-06-06 16:21:56 +0300

    colors stack

--- AdminShopify/AdminShopify/Modules/EditableProductDetais/view/EditableProductDetailsViewController.swift
+++ AdminShopify/AdminShopify/Modules/EditableProductDetais/view/EditableProductDetailsViewController.swift
@@ -8,56 +8,78 @@
 import UIKit
 
 class EditableProductDetailsViewController: UIViewController {
-
+    
     @IBOutlet weak var imgCollectionView: UICollectionView!
     @IBOutlet weak var pageController: UIPageControl!
+    
     @IBOutlet weak var sizeScrollable: UIScrollView!
     @IBOutlet weak var sizeStackView: UIStackView!
+    
+    @IBOutlet weak var colorScrollStackView: UIScrollView!
+    @IBOutlet weak var colorView: UIStackView!
+    
     var arrProductImg = [UIImage(named: "addidus") , UIImage(named: "unnamed") , UIImage(named: "lock") ,UIImage(named: "addidus") ,UIImage(named: "unnamed") ]
     var timer : Timer?
     var currentCellIndex = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         pageController.numberOfPages = arrProductImg.count
         startTimer()
         let sizes = ["Small", "Medium", "Large", "XL", "XXL", "XXXL", "US 6", "US 8", "US 10", "US 12"]
-            
-            for size in sizes {
-                addSize(size: size)
-            }
+        
+        for size in sizes {
+            addSize(size: size)
+        }
+        
+        let colors = ["Red", "Blue", "Green", "Yellow", "Orange", "Purple", "Black", "White", "Gray", "Pink"]
+        for color in colors {
+            addColor(color: color)
+        }
     }
     
     func startTimer(){
         timer = Timer.scheduledTimer(timeInterval: 2, target: self, selector:#selector(moveToNextProductImg) , userInfo: nil, repeats: true)
     }
     
-   @objc func moveToNextProductImg(){
-       if currentCellIndex < arrProductImg.count - 1 {
-           currentCellIndex += 1
-       }else{
-           currentCellIndex = 0
-       }
-       imgCollectionView.scrollToItem(at: IndexPath(item: currentCellIndex, section: 0), at: .centeredHorizontally, animated: true)
-       pageController.currentPage = currentCellIndex
+    @objc func moveToNextProductImg(){
+        if currentCellIndex < arrProductImg.count - 1 {
+            currentCellIndex += 1
+        }else{
+            currentCellIndex = 0
+        }
+        imgCollectionView.scrollToItem(at: IndexPath(item: currentCellIndex, section: 0), at: .centeredHorizontally, animated: true)
+        pageController.currentPage = currentCellIndex
     }
     
+    func styleView(_ view: UIView) {
+        view.layer.cornerRadius = 10
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.gray.cgColor
+        view.layer.shadowColor = UIColor.gray.cgColor
+        view.layer.shadowOpacity = 0.5
+        view.layer.shadowOffset = CGSize(width: 4, height: 2)
+        view.layer.shadowRadius = 4
+    }
+
+    func styleLabel(_ label: UILabel) {
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: UIFont.labelFontSize, weight: .regular)
+    }
+
+
     func addSize(size: String) {
         let newSizeView = UIView()
         newSizeView.backgroundColor = UIColor.white
-        
-        newSizeView.layer.shadowColor = UIColor.gray.cgColor
-        newSizeView.layer.shadowOpacity = 0.5
-        newSizeView.layer.shadowOffset = CGSize(width: 4, height: 2)
-        newSizeView.layer.shadowRadius = 4
+        styleView(newSizeView)
         
         let newSizeLabel = UILabel()
         newSizeLabel.text = size
-        newSizeLabel.textColor = UIColor.black
+        styleLabel(newSizeLabel)
         
         newSizeView.addSubview(newSizeLabel)
-        
         newSizeLabel.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
             newSizeLabel.topAnchor.constraint(equalTo: newSizeView.topAnchor, constant: 8),
             newSizeLabel.leadingAnchor.constraint(equalTo: newSizeView.leadingAnchor, constant: 8),
@@ -67,8 +89,28 @@ class EditableProductDetailsViewController: UIViewController {
         
         sizeStackView.addArrangedSubview(newSizeView)
     }
-    
-   
+
+    func addColor(color: String) {
+        let newColorView = UIView()
+        newColorView.backgroundColor = UIColor(named: color)
+        styleView(newColorView)
+        
+        let newColorLabel = UILabel()
+        newColorLabel.text = color
+        styleLabel(newColorLabel)
+        
+        newColorView.addSubview(newColorLabel)
+        newColorLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            newColorLabel.topAnchor.constraint(equalTo: newColorView.topAnchor, constant: 8),
+            newColorLabel.leadingAnchor.constraint(equalTo: newColorView.leadingAnchor, constant: 8),
+            newColorLabel.trailingAnchor.constraint(equalTo: newColorView.trailingAnchor, constant: -8),
+            newColorLabel.bottomAnchor.constraint(equalTo: newColorView.bottomAnchor, constant: -8)
+        ])
+        
+        colorView.addArrangedSubview(newColorView)
+    }
 
 }
 
